7f0164913b1c3a40ca8bec0c3a70420e97dc98a3
Jiwon Kang <jwkorange@gmail.com>
2019-12-18 16:44:27 +0900

    26.Instancing result & source code uploaded

---
+++ Sources/26.Instancing/shader_s.h
@@ -0,0 +1,34 @@
+#pragma once
+#ifndef SHADER_H
+#define SHADER_H
+
+#include <glad/glad.h>
+
+#include <glm/glm/glm.hpp>
+#include <glm/glm/gtc/type_ptr.hpp>
+
+#include <string>
+#include <fstream>
+#include <sstream>
+#include <iostream>
+
+
+class Shader
+{
+public:
+	GLuint ID;
+
+	// constructor reads and builds the shader
+	Shader(const GLchar* vertexPath, const GLchar* fragmentPath);
+	//use/activate the shader
+	void use();
+	// utility uniform functions
+	void setBool(const std::string &name, bool value) const;
+	void setInt(const std::string &name, int value) const;
+	void setFloat(const std::string &name, float value) const;
+	void setMat4(const std::string &name, glm::mat4 value) const;
+	void setVec2(const std::string &name, glm::vec2 value) const;
+	void setVec3(const std::string &name, glm::vec3 value) const;
+};
+
+#endif
